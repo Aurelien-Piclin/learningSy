873d71b0ba09cfd23d66fbecccb5d19d4f3d635b
a_piclin <piclinaurelien@gmail.com>
2023-11-07 14:19:09 +0100

    workflow, notification, bootstrap

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,13 +14,15 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-        private MessageBusInterface $bus
+        private MessageBusInterface    $bus
     )
     {
     }
@@ -30,11 +32,20 @@ class ConferenceController extends AbstractController
     {
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
+        ])->setSharedMaxAge(3600);
+    }
+
+    #[Route('/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository): Response
+    {
+        return $this->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
         ]);
     }
 
+
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, #[Autowire('%photo_dir%')] string $photoDir): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, NotifierInterface $notifier, #[Autowire('%photo_dir%')] string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
@@ -61,7 +72,13 @@ class ConferenceController extends AbstractController
 
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -36,25 +36,41 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 
-   public function testCommentSubmission()
-   {
-       $client = static::createClient();
-       $client->request('GET', '/conference/amsterdam-2019');
-       $client->submitForm('Submit', [
-           'comment[author]' => 'Fabien',
-           'comment[text]' => 'Some feedback from an automated functional test',
-           'comment[email]' => $email = 'me@automat.ed',
-           'comment[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
-       ]);
-       $this->assertResponseRedirects();
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment[author]' => 'Fabien',
+            'comment[text]' => 'Some feedback from an automated functional test',
+            'comment[email]' => $email = 'me@automat.ed',
+            'comment[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
 
-       // simulate comment validation
-               $comment = self::getContainer()->get(CommentRepository::class)->findOneByEmail($email);
-               $comment->setState('published');
-               self::getContainer()->get(EntityManagerInterface::class)->flush();
+        // simulate comment validation
+        $comment = self::getContainer()->get(CommentRepository::class)->findOneByEmail($email);
+        $comment->setState('published');
+        self::getContainer()->get(EntityManagerInterface::class)->flush();
 
-       $client->followRedirect();
-       $this->assertSelectorExists('div:contains("There are 2 comments")');
-  }
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
+
+    }
+
+    public function testMailerAssertions()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertEmailCount(1);
+        $event = $this->getMailerEvent(0);
+        $this->assertEmailIsQueued($event);
+
+        $email = $this->getMailerMessage(0);
+        $this->assertEmailHeaderSame($email, 'To', 'fabien@example.com');
+        $this->assertEmailTextBodyContains($email, 'Bar');
+        $this->assertEmailAttachmentCount($email, 1);
+    }
 
 }
